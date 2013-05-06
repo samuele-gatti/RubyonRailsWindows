9b62eb4758bc92dac3bf06b76c68e99b7b849f40
Samuele Gatti <samuele.gatti@studiofarma.it>
2013-05-06 17:05:10 +0200

    Finalmente riusciamo a salvare nuove Piadinerie con MongoDb

---
+++ PiadineriaAPI/app/controllers/piadineria_controller.rb
@@ -0,0 +1,83 @@
+class PiadineriaController < ApplicationController
+  # GET /piadineria
+  # GET /piadineria.json
+  def index
+    @piadineria = Piadinerium.all
+
+    respond_to do |format|
+      format.html # index.html.erb
+      format.json { render json: @piadineria }
+    end
+  end
+
+  # GET /piadineria/1
+  # GET /piadineria/1.json
+  def show
+    @piadinerium = Piadinerium.find(params[:id])
+
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @piadinerium }
+    end
+  end
+
+  # GET /piadineria/new
+  # GET /piadineria/new.json
+  def new
+    @piadinerium = Piadinerium.new
+
+    respond_to do |format|
+      format.html # new.html.erb
+      format.json { render json: @piadinerium }
+    end
+  end
+
+  # GET /piadineria/1/edit
+  def edit
+    @piadinerium = Piadinerium.find(params[:id])
+  end
+
+  # POST /piadineria
+  # POST /piadineria.json
+  def create
+    @piadinerium = Piadinerium.new(params[:piadinerium])
+
+    respond_to do |format|
+      if @piadinerium.save
+        format.html { redirect_to @piadinerium, notice: 'Piadinerium was successfully created.' }
+        format.json { render json: @piadinerium, status: :created, location: @piadinerium }
+      else
+        format.html { render action: "new" }
+        format.json { render json: @piadinerium.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # PUT /piadineria/1
+  # PUT /piadineria/1.json
+  def update
+    @piadinerium = Piadinerium.find(params[:id])
+
+    respond_to do |format|
+      if @piadinerium.update_attributes(params[:piadinerium])
+        format.html { redirect_to @piadinerium, notice: 'Piadinerium was successfully updated.' }
+        format.json { head :no_content }
+      else
+        format.html { render action: "edit" }
+        format.json { render json: @piadinerium.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  # DELETE /piadineria/1
+  # DELETE /piadineria/1.json
+  def destroy
+    @piadinerium = Piadinerium.find(params[:id])
+    @piadinerium.destroy
+
+    respond_to do |format|
+      format.html { redirect_to piadineria_url }
+      format.json { head :no_content }
+    end
+  end
+end
---
+++ PiadineriaAPI/app/models/piadinerium.rb
@@ -0,0 +1,12 @@
+class Piadinerium 
+ include MongoMapper::Document
+
+key :ragioneSociale, String
+key :indirizzo, String
+key :comune, String
+key :provincia, String
+key :regione, String
+key :latitudine, String
+key :longitudine, String
+
+end
