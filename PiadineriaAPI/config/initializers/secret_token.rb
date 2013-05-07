80f04dd249e17440074db10e42febf3948eaa5e1
Samuele Gatti <samuele.gatti@studiofarma.it>
2013-05-07 08:52:17 +0200

    Rimosso il precedente progetto

--- PiadineriaAPI/config/initializers/secret_token.rb
+++
@@ -1,7 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-# Your secret key for verifying the integrity of signed cookies.
-# If you change this key, all old signed cookies will become invalid!
-# Make sure the secret is at least 30 characters and all random,
-# no regular words or you'll be exposed to dictionary attacks.
-PiadineriaAPI::Application.config.secret_token = '92a3dacbd0a4382220586bc46f34a82ed4b22043d47ef5e3f88fd749f3bcdd74289a6b2c987da89bfe5c7d641c5ca4e6bf38ff686001655a8b86138e63eac054'
--- PiadineriaAPI/config/initializers/session_store.rb
+++
@@ -1,8 +0,0 @@
-# Be sure to restart your server when you modify this file.
-
-PiadineriaAPI::Application.config.session_store :cookie_store, key: '_PiadineriaAPI_session'
-
-# Use the database for sessions instead of the cookie-based default,
-# which shouldn't be used to store highly confidential information
-# (create the session table with "rails generate session_migration")
-# PiadineriaAPI::Application.config.session_store :active_record_store
