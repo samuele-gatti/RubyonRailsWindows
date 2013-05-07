1bbd869a791c81195c4ba1f49c2b39add510b93b
Samuele Gatti <samuele.gatti@studiofarma.it>
2013-05-07 08:54:11 +0200

    Nuovo progetto

---
+++ PiadAPI/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+PiadAPI::Application.config.secret_token = 'db12b2ae9eb2da6ed65f3516e74fcbeabef3f81e647953f4668d42c2002d829b328df0603bd17a6558eb6619803532d0c39f99a3889af9fc74852e6a105ef629'
