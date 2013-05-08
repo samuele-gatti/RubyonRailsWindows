78279db8de5c629ccc4487ea9b4339d9850e8dee
Rails user <railsuser@RubyonRails.0d976711eff744c8b8626efbdaefb121.RailsService.2258635155.europewest.internal.cloudapp.net>
2013-05-08 14:10:28 +0000

    adding config files

---
+++ PiadAPI/config/unicorn.rb
@@ -0,0 +1,12 @@
+
+rker_processes 2
+timeout 30
+listen "/tmp/unicorn.blog.sock"
+ 
+root = "/home/railsuser/sites/PiadAPI"
+ 
+working_directory root
+ 
+pid "#{root}/tmp/pids/unicorn.pid"
+stderr_path "#{root}/log/unicorn.log"
+stdout_path "#{root}/log/unicorn.log"
